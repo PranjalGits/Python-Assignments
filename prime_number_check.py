0d76587321594e51636aea2d33eacc6a5ea403d8
PranjalGits <133793026+PranjalGits@users.noreply.github.com>
2023-05-22 08:46:26 -0700

    Python_2nd_Program

---
+++ prime_number_check.py
@@ -0,0 +1,14 @@
+# take input from the user
+num = int(input("Enter a number: "))
+
+# check if the number is prime
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
