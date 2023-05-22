324ec2c09e2dc97853e173cd1724370c007290cc
PranjalGits <133793026+PranjalGits@users.noreply.github.com>
2023-05-22 08:43:42 -0700

    Python_1st_Program

---
+++ odd_even_check.py
@@ -0,0 +1,8 @@
+# take input from the user
+num = int(input("Enter an integer: "))
+
+# check if the number is even or odd
+if num % 2 == 0:
+    print(num, "is even")
+else:
+    print(num, "is odd")
