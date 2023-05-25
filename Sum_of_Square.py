bd2b59ed53952baf60acfae5feb0aac9b665b3e1
PranjalGits <133793026+PranjalGits@users.noreply.github.com>
2023-05-24 21:39:33 -0700

    Python_8th_Program

---
+++ Sum_of_Square.py
@@ -0,0 +1,7 @@
+n = 5  # Replace with the desired value of n
+
+sum_of_squares = 0
+for i in range(1, n+1):
+    sum_of_squares += i*i
+
+print(f"The sum of squares of first {n} natural numbers is: {sum_of_squares}")
