8222a9d699a5364c1cf8138307c32dd3f52dcf6e
PranjalGits <133793026+PranjalGits@users.noreply.github.com>
2023-06-01 10:47:47 -0700

    Create Largest_number_in_the_list.py

---
+++ Largest_number_in_the_list.py
@@ -0,0 +1,9 @@
+# Python program to find smallest
+# number in a list
+ 
+# list of numbers
+list1 = [10, 20, 1, 45, 99]
+ 
+ 
+# printing the maximum element
+print("Largest element is:", max(list1))
