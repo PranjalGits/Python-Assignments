bb8f1077cf4585de9f8f6bf15fe2e717cba269d4
PranjalGits <133793026+PranjalGits@users.noreply.github.com>
2023-06-01 11:43:25 -0700

    Create read_file_character_by_character.py

---
+++ read_file_character_by_character.py
@@ -0,0 +1,15 @@
+# to read file character by character
+ 
+ 
+file = open('file.txt', 'r')
+ 
+while 1:
+     
+    # read by character
+    char = file.read(1)         
+    if not char:
+        break
+         
+    print(char)
+ 
+file.close()
