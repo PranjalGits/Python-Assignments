84bc7bdebab5747dd5dc1b866ebdd23b884475d2
PranjalGits <133793026+PranjalGits@users.noreply.github.com>
2023-06-01 10:33:41 -0700

    Create size_of _tuple.py

---
+++ size_of _tuple.py
@@ -0,0 +1,11 @@
+# Python program to find the size of a tuple 
+
+# Creating a tuple in python 
+myTuple = ('include', 'python', 3, 2021)
+
+# Finding size of tuple
+tupleSize = myTuple._sizeof_()
+
+# Printing the tuple and size
+print("Tuple : ", str(myTuple))
+print("Tuple Length : ", tupleSize)
